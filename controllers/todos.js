7fd04ae9af0068d78fd9b4123e084215253b08f4
Jason Serafica <seraficaj@gmail.com>
2023-07-06 10:14:41 -0700

    https://git.generalassemb.ly/SEI-Rancho-Cordova/SEI_Rancho_Cordova/blob/main/Unit_2/W04/D18/18.1-ejs-partial-templates.md\#code-headerejs

--- controllers/todos.js
+++ controllers/todos.js
@@ -11,11 +11,13 @@ const Todo = require("../models/todo");
 function index(req, res) {
     res.render("todos/index", {
         todos: Todo.getAll(),
+        title: "All To-Dos"
     });
 }
 
 function show(req, res) {
     res.render("todos/show", {
         todo: Todo.getOne(req.params.id),
+        title: "To-Do Details"
     });
 }
